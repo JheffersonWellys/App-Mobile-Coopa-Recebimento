23f99391643314b249bea8ae520901f81edec8f4
jhefferson <jhefferson.w.dev@gmail.com>
2024-07-26 18:52:57 -0300

    feat(login): adiciona nova tela Act_Login
    Adiciona um novo arquivo de design e código fonte para a tela de
    login.

---
+++ app/src/main/java/com/h4rzel/coopa_recebimento/Act_Login.java
@@ -0,0 +1,14 @@
+package com.h4rzel.coopa_recebimento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Act_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_login);
+    }
+}
