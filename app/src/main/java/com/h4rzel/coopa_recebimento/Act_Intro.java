a1c2403acc0b62f603a5ab534052feb43eb7b2cb
jhefferson <jhefferson.w.dev@gmail.com>
2024-07-26 19:29:58 -0300

    fix(redirect): adiciona redirecionamento automático
    Adiciona um redirecionamento automático com temporizador da tela
    intro para a tela de logon.

--- app/src/main/java/com/h4rzel/coopa_recebimento/Act_Intro.java
+++ app/src/main/java/com/h4rzel/coopa_recebimento/Act_Intro.java
@@ -2,7 +2,9 @@ package com.h4rzel.coopa_recebimento;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Act_Intro extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class Act_Intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_intro);
+
+        // Chama o redirecionamento automático com temporizador para tela de logon
+        RedirecionamentoDeTela();
+
     }
+
+    private void RedirecionamentoDeTela(){
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Act_Intro.this, Act_Logon.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },2000);
+
+    }
+
 }
