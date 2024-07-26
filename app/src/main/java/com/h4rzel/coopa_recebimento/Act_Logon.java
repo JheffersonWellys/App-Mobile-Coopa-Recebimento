1ae11c98a0ec001b58b13f0a350c28f0e73af379
jhefferson <jhefferson.w.dev@gmail.com>
2024-07-26 19:21:00 -0300

    refactor(login): renomeia Act_Login para Act_Logon
    Renomeia a classe Act_Login para Act_Logon para refletir melhor
    seu propósito e melhorar a organização do código. Também foram
    ajustadas as referências para garantir a consistência no projeto.

--- app/src/main/java/com/h4rzel/coopa_recebimento/Act_Logon.java
+++ app/src/main/java/com/h4rzel/coopa_recebimento/Act_Logon.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Act_Login extends AppCompatActivity {
+public class Act_Logon extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_login);
+        setContentView(R.layout.act_logon);
     }
 }
