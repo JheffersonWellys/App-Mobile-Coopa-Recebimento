0ec9e00dcda4d3c98024ef8a891022f29b5fe115
jhefferson <jhefferson.w.dev@gmail.com>
2024-07-26 20:13:49 -0300

    fix(login): altera nome design layout fragment login
    Altera o nome do arquivo de design frag__login.xml para
    frag_login.xml e modifica arquivos relacionados para refletir
    as mudanças.

--- app/src/main/java/com/h4rzel/coopa_recebimento/Frag_Login.java
+++ app/src/main/java/com/h4rzel/coopa_recebimento/Frag_Login.java
@@ -59,6 +59,6 @@ public class Frag_Login extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frag__login, container, false);
+        return inflater.inflate(R.layout.frag_login, container, false);
     }
 }
