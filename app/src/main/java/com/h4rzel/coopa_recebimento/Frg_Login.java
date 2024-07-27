ee06e2a04bb0371a0abe57ae94a9b92fe2c99a22
jhefferson <jhefferson.w.dev@gmail.com>
2024-07-26 21:11:55 -0300

    fix(login): altera os nomes dos arquivos da tela login
    Altera os nomes do arquivo de design e código fonte da tela
    de login.

--- app/src/main/java/com/h4rzel/coopa_recebimento/Frg_Login.java
+++ app/src/main/java/com/h4rzel/coopa_recebimento/Frg_Login.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Frag_Login#newInstance} factory method to
+ * Use the {@link Frg_Login#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Frag_Login extends Fragment {
+public class Frg_Login extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Frag_Login extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Frag_Login() {
+    public Frg_Login() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class Frag_Login extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Frag_Login.
+     * @return A new instance of fragment Frg_Login.
      */
     // TODO: Rename and change types and number of parameters
-    public static Frag_Login newInstance(String param1, String param2) {
-        Frag_Login fragment = new Frag_Login();
+    public static Frg_Login newInstance(String param1, String param2) {
+        Frg_Login fragment = new Frg_Login();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Frag_Login extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frag_login, container, false);
+        return inflater.inflate(R.layout.frg_login, container, false);
     }
 }
